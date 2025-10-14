27baddf95c2154c506cc9c9b7bb5024e3adfe2ef
Simon Forsling <simon.forsling@xertified.com>
2025-10-14 20:28:12 +0200

    More pedantic flags

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ if [ "$1" = "--debug" ] || [ "$1" = "-g" ]; then
     DEBUG=1
 fi
 
-CFLAGS="-std=c11 -Wall -Wextra -Wconversion -Wshadow -Wcast-qual -Wpointer-arith -Wbad-function-cast -Wundef"
+CFLAGS="-std=c11 -Wall -Werror -Wextra -Wconversion -Wshadow -Wcast-qual -Wpointer-arith -Wbad-function-cast -Wundef -pedantic -Wcast-align -Wmissing-declarations -Wwrite-strings -Wstrict-aliasing=2"
 if [ $DEBUG -eq 1 ]; then
     CFLAGS="$CFLAGS -g -O0 -DDEBUG"
 else
@@ -17,8 +17,8 @@ else
 fi
 
 echo "Building fiskta $VERSION..."
+set -x
 $CC $CFLAGS -DFISKTA_VERSION=\"$VERSION\" -D_POSIX_C_SOURCE=199309L \
     src/main.c src/cli_help.c src/parse.c src/engine.c src/iosearch.c src/reprog.c src/util.c \
     -o fiskta
 
-echo "Built ./fiskta"
