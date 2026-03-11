a878276f2d6a73e1d7ab83dff52b8e9c1195fe90
Simon Forsling <simon.forsling@xertified.com>
2026-03-11 22:09:07 +0100

    Remove redundant execution API banner

--- src/fiskta.h
+++ src/fiskta.h
@@ -109,9 +109,6 @@ typedef struct {
     void* output_userdata;
 } FisktaRuntimeConfig;
 
-/*****************************
- * THREE-PHASE EXECUTION API *
- *****************************/
 // FisktaRuntimeBuffers: All execution-time working memory for one FisktaProgram.
 //
 // Lifetime:
